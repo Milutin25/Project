eff92adf689676f2ab49b8454b5428d5f93c9b3c
Milutin25 <stefanovicfamily@Stefanovics-iMac.local>
2024-03-08 17:38:23 +0100

    Mouse Hover

--- main.js
+++ main.js
@@ -2,6 +2,12 @@ const button = document.getElementById("btn");
 
 button.addEventListener("click", function onClick () {
 
-button.style.backgroundColor = "blue";
+button.style.backgroundColor = "rgb(19, 212, 196)";
 button.style.transition = "2s ease";
 });
+button.addEventListener("mouseover", ()=>{
+    button.style.backgroundColor = "rgb(191, 78, 78)";
+})
+button.addEventListener("mouseout", ()=>{
+    button.style.backgroundColor = "rgba(255, 100, 100, 1)";
+})
